eb863088f099f6df5fde185c9380bf34e06d6315
Larry Wang <lawrence.wang@datadoghq.com>
2025-11-09 19:02:09 -0500

    Gamestate playable

--- GameState.py
+++ GameState.py
@@ -5,6 +5,7 @@ class GameState:
         self.player2_hands = sorted([opponent_low_hand, opponent_high_hand])
 
     def move(self, action: str, action_params: list):
+
         if action == 'tap':
             attacking_hand_index = action_params[0]
             target_hand_index = action_params[1]
@@ -26,13 +27,16 @@ class GameState:
         if action == 'split':
             new_hand_1 = action_params[0]
             new_hand_2 = action_params[1]
-
             if self.player1_turn:
-                self.player1_hands = sorted([new_hand_1, new_hand_2])
-
+                self.player1_hands = [new_hand_1, new_hand_2]
             else:
-                self.player2_hands = sorted([new_hand_1, new_hand_2])
-        
+                self.player2_hands = [new_hand_1, new_hand_2]
+
+            print("entered")
+
+        self.player1_hands.sort()
+        self.player2_hands.sort()
+            
         # change to next turn
         self.player1_turn = not self.player1_turn
 
@@ -53,7 +57,6 @@ class GameState:
                 attacking_num = self.player2_hands[attacking_hand_index]
                 target_num = self.player1_hands[target_hand_index]
 
-
             if attacking_num == 0:
                 print(f'Attacking with an empty hand index {attacking_hand_index}')  
                 return False
@@ -61,25 +64,81 @@ class GameState:
             if target_num == 0:
                 print(f'Target is an empty hand index {target_hand_index}')  
                 return False
+
             
         if action == "split":
+
             hand1, hand2 = action_params
 
             if not (0<hand1<=5 and 0<hand2<=5):
                 print('Distributed values must be in between 0<x<=5')  
                 return False
-            
+
             current_hand = self.player1_hands if self.player1_turn else self.player2_hands
 
-            if (current_hand)!=sum(action_params):
-                print(f'Starting and ending of distribution must equal. {current_hand}!={sum(action_params)}')  
+            if (sum(current_hand))!=sum(action_params):
+                print(f'Starting and ending of distribution must be equal. {sum(current_hand)}!={sum(action_params)}')  
                 return False
-            
+
+            if sorted(current_hand)==sorted(action_params):
+                print(f'No duplicate swapping')  
+                return False
 
         return True
-    
 
+    def is_over(self):
 
+        if sum(self.player1_hands) == 0:
+            print('Player Two Wins!')
+            return True
+        if sum(self.player2_hands) == 0:
+            print('Player One Wins!')
+            return True
+
+        return False
+
+    def __repr__(self) -> str:
+        output = "P1 turn" if self.player1_turn else "P2 turn"
+        output += f' Player 1 hands: {self.player1_hands} Player 2 hands: {self.player2_hands}'
+        return output
 
 def main():
-    pass
+    
+    game = GameState()
+
+
+    while (not game.is_over()):
+
+        print(game)
+        # human player move
+        if game.player1_turn:
+            user_input = input('Insert action type, param1, and param 2 \n').split()
+            if (len(user_input)!=3):
+                print('Invalid input, must be three')
+                continue
+
+            move_type, param1, param2 = user_input
+            param1, param2 = int(param1), int(param2)
+            if not game.is_valid_move(move_type, [param1, param2]):
+                continue
+            game.move(move_type, [param1, param2])
+
+
+        # second player move
+        else:
+            user_input = input('Insert action type, param1, and param 2 \n').split()
+            if (len(user_input)!=3):
+                print('Invalid input, must be three')
+                continue
+
+            move_type, param1, param2 = user_input
+            param1, param2 = int(param1), int(param2)
+            if not game.is_valid_move(move_type, [param1, param2]):
+                continue
+            game.move(move_type, [param1, param2])
+
+        print()
+        
+    
+if __name__ == "__main__":
+    main()
