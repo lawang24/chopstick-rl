ff512d548f9050ca23f4f8c409238e87cdab6772
Lawrence Wang <lawrence.wang24@gmail.com>
2026-01-22 19:51:49 -0500

    fix types

--- custom_types.py
+++ custom_types.py
@@ -1,11 +1,10 @@
 from enum import Enum
 from typing import Literal, TypeAlias
 
-class Actions(str, Enum):
+class Actions(Enum):
     tap = 'tap'
     split = 'split'
 
 ActionType = Literal[Actions.tap, Actions.split]
 Move: TypeAlias = tuple[ActionType, tuple[int, int]]
 GamePosition: TypeAlias = tuple[tuple[int, int], tuple[int, int]]
-
